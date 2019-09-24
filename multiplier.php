50fa02db7421c09268fab2fe87ab723808504cf2
Randy Brouwer <2105070@talnet.nl>
2019-09-24 10:03:32 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hewwo my name is Mumfwr!! Whats your first numbwer OwO???   ";
+$Number1 = readline();
+echo "And wuts the second numbwer? OwO?   ";
+$Number2 = readline();
+echo "Your awnser is ";
+echo $Number1 * $Number2;
+echo ", Big UwU!!";
+echo " Gimme your buldgy wuldgy pweashee!!";
